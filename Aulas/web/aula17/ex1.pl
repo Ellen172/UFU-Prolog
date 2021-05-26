4af22ec45eea85221cb6777fc15d64d211044f71
EllenCas172 <ellencas172@gmail.com>
2021-05-26 20:43:12 -0300

    aula 26-05

--- Aulas/web/aula17/ex1.pl
+++ Aulas/web/aula17/ex1.pl
@@ -4,26 +4,20 @@
 
 % html_requires está aqui
 :- use_module(library(http/html_head)).
-
 % serve_files_in_directory está aqui
 :- use_module(library(http/http_server_files)).
-
 % http_read_data está aqui
 :- use_module(library(http/http_client)).
 
 servidor(Porta) :-
     http_server(http_dispatch, [port(Porta)]).
 
-
-
 % Liga a rota ao tratador
-:- http_handler(root(.), formulário , []).
+:- http_handler(root(.), formulario , []).
 
-
-
-formulário(_Pedido) :-
+formulario(_Pedido) :-
 	reply_html_page(
-	    title('Demonstração de POST'),
+	    title('Demonstracao de POST'),
 	    [ form([ action='/receptor', method='POST'],
 	           [ p([],
 	               [ label([for=name],'Nome:'),
@@ -36,15 +30,15 @@ formulário(_Pedido) :-
                          []))
 	           ])]).
 
-:- http_handler('/receptor', recebe_formulário(Method),
+:- http_handler( '/receptor', recebe_formulario(Method),
                 [ method(Method),
-                  methods([post]) ]).
+                  methods([post]) ] ).
 
-recebe_formulário(post, Pedido) :-
-    http_read_data(Pedido, Dados, []),
+recebe_formulario(post, Pedido) :- % será executado após clicar no botão 'enviar'
+    http_read_data(Pedido, Dados, []), % Dados retorna os valores input do formulario (o que foi inserido)
     format('Content-type: text/html~n~n', []),
 	format('<p>', []),
     portray_clause(Dados), % escreve os dados do corpo
-	format('</p><p>========~n', []),
-	portray_clause(Pedido), % escreve o pedido todo
+	format('</p><h2>Obrigado por Enviar!</h2><p>========~n', []),
+	portray_clause(Pedido), % escreve o pedido todo (o que chega no servidor)
 	format('</p>').
--- Aulas/web/aula17/ex2.pl
+++ Aulas/web/aula17/ex2.pl
@@ -8,8 +8,6 @@
 servidor(Porta) :-
     http_server(http_dispatch, [port(Porta)]).
 
-
-
 % Liga a rota ao tratador
 :- http_handler(root(.), home , []).
 
@@ -20,46 +18,40 @@ servidor(Porta) :-
 */
 
 home(Pedido) :-
-	reply_html_page( title('Demonstração de GET'),
-		           [ \página(Pedido) ]).
+	reply_html_page( title('Demonstracao de GET'),
+		           [ \pagina(Pedido) ]).
 
-página(Pedido) -->
-    {
-        /* É preciso tratar a exceção, pois http_parameters gera uma
-           uma exceção caso um parâmetro seja inválido */
-        catch(
-            http_parameters(Pedido,
-                            [ % id dever ser um inteiro
-                                id(Id,          [integer]),
-                                nome(Nome,      [string]),
-                                prof(Profissão, [string]),
-                              /* Se o parâmetro idade estiver
-                                 ausente, Idade não ficará ligada
-                               */
-                                idade(Idade,    [optional(true),integer]),
-                              /* Se o parâmetro nacionalidade estiver
-                                 ausente, assuma o valor default.
-                              */
-                                nacionalidade(Nac, [default(brasileira)])
-                            ]),
-            _E,
-            fail ),
-        !,
+pagina(Pedido) -->
+  {
+  % É preciso tratar a exceção, pois http_parameters gera uma exceção caso um parâmetro seja inválido 
+    catch(
+      http_parameters(Pedido,
+                      [ % id dever ser um inteiro
+                        id(Id,          [integer]),
+                        nome(Nome,      [string]),
+                        prof(Profissao, [string]),
+                        % Se o parâmetro idade estiver ausente, Idade não ficará ligada
+                        idade(Idade,    [optional(true),integer]),
+                        % Se o parâmetro nacionalidade estiver ausente, assuma o valor default.
+                        nacionalidade(Nac, [default(brasileira)])
+                      ]),
+            _E, % armazena a exceção , o que não estiver indicado nas [] anteriores.
+            fail ), !,
         % Verifica se a idade foi informada
         (var(Idade) -> Idade = 'desconhecida' ; true)
     },
     html([ h1('Resposta do servidor para o GET'),
-           p('Os parâmetros recebidos foram:'),
-           p('Id é ~w' - Id),
-           p('Nome é ~w' - Nome),
-           p('Profissão é ~w'- Profissão),
-           p('Idade é ~w' - Idade),
-           p('Nacionalidade é ~w'- Nac)
+           p('Os parametros recebidos foram:'),
+           p('Id e ~w' - Id),
+           p('Nome e ~w' - Nome),
+           p('Profissao e ~w'- Profissao),
+           p('Idade e ~w' - Idade),
+           p('Nacionalidade e ~w'- Nac)
          ]).
 
-/* Essa página será exibida em caso de erro de validação
+/* Essa pagina será exibida em caso de erro de validação
    de algum parâmetro */
-página(_Pedido) -->
+pagina(_Pedido) -->
     html([ h1('Erro'),
-           p('Algum parâmetro não é válido')
+           p('Algum parametro nao e valido')
          ]).
