2f8d8bfaec17e5107668599cb3c0c4535cbc30e8
EllenCas172 <ellencas172@gmail.com>
2021-05-26 19:35:51 -0300

    modificações no trabalho

--- Aulas/banco de dados/departamento.pl
+++ Aulas/banco de dados/departamento.pl
@@ -1,7 +1,7 @@
-/* Departamento                        */
-/*         NumDepto, Nome, MatGerente  */
+/* Departamento */
+/* NumDepto, Nome, MatGerente  */
 
 :- dynamic departamento/3.
 
-departamento(1, 'Relações Públicas', 21).
+departamento(1, 'Relações Públicas', 21). % nome(dado1, dado2, dado3).
 departamento(2, 'Segurança',         43).
