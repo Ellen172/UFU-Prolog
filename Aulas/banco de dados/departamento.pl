d5508c5c310c1e0595085f3986993e447b749dbc
EllenCas172 <ellencas172@gmail.com>
2021-05-27 14:52:25 -0300

    organizando arquivos

--- Aulas/banco de dados/departamento.pl
+++
@@ -1,7 +0,0 @@
-/* Departamento */
-/* NumDepto, Nome, MatGerente  */
-
-:- dynamic departamento/3.
-
-departamento(1, 'Relações Públicas', 21). % nome(dado1, dado2, dado3).
-departamento(2, 'Segurança',         43).
